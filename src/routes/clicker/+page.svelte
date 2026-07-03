8b765071a391ce81738d6d680f41f1ede05ce1e3
Sharky <es1dorow@yandex.ru>
2026-07-03 15:13:12 +0300

    simple clicker

--- src/routes/clicker/+page.svelte
+++ src/routes/clicker/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	import Nav from '../components/Nav.svelte';
+	let count = $state(0);
+
+	function increment() {
+		count += 1;
+	}
+	function decrement() {
+		count -= 1;
+	}
 </script>
 
 <main>
 	<Nav />
-
-	<button type="button">click me</button>
+	<p>
+		Clicked {count}
+		{count === 1 ? 'time ' : 'times'}
+	</p>
+	<button type="button" onclick={increment}>+1</button>
+	<button type="button" onclick={decrement}>-1</button>
 </main>
